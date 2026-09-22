f7b48dffdac00a78511c2d817e4d19be1a1f2bb1
flyer62 <cfritz3@ameritech.net>
2026-09-21 22:42:41 -0500

    Enhance smoothie app with search functionality
    Updated the data retrieval process to include 'SEARCH_ON' column and modified API call to use search value.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests  
+import requests
+import pandas as pd
 
 
 # Write directly to the app
@@ -16,7 +17,7 @@ st.write("The name on the Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+pd_df = my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 
@@ -31,9 +32,12 @@ if ingredients_list:
     ingredients_string =''
 
     for fruit_chosen in ingredients_list:
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'search_on'].iloc[0]
         ingredients_string += fruit_chosen + ' '
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + ' Nutrition Information')    
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
