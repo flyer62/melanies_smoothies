25392af01249a69faab7d35855a027d1ead71561
flyer62 <cfritz3@ameritech.net>
2026-09-21 22:46:46 -0500

    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 pd_df = my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 
@@ -32,8 +33,8 @@ if ingredients_list:
     ingredients_string =''
 
     for fruit_chosen in ingredients_list:
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'search_on'].iloc[0]
         ingredients_string += fruit_chosen + ' '
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')    
